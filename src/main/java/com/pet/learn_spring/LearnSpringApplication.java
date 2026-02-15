12c25670f8eea6bd18739215fbb6583133d4bfe1
Mihail <mishavlashitschii@gmail.com>
2026-02-15 12:47:50 +0200

    SP-1 ADD FileSystem.java file keeper     * FileSystem.java writes and reads files into the file system

--- src/main/java/com/pet/learn_spring/LearnSpringApplication.java
+++ src/main/java/com/pet/learn_spring/LearnSpringApplication.java
@@ -1,18 +1,20 @@
 package com.pet.learn_spring;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import com.pet.learn_spring.core.FileSystem;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.util.unit.DataSize;
 
 @SpringBootApplication
 public class LearnSpringApplication {
-	private static final Logger loggerStatic =
-			LoggerFactory.getLogger(LearnSpringApplication.class);
-	private final Logger logger = LoggerFactory.getLogger(getClass());
-
 	public static void main(String[] args) {
 		ApplicationContext app = SpringApplication.run(LearnSpringApplication.class, args);
+
+		FileSystem fileSystem = app.getBean(FileSystem.class);
+
+		System.out.println(
+				DataSize.ofBytes(fileSystem.getFreeDiskSpace()).toGigabytes() + " GB"
+		);
 	}
 }
