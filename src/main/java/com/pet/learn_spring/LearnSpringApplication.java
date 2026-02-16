91ee2df4a7472dec4f1ecec7070a7180c92df447
Mihail <mishavlashitschii@gmail.com>
2026-02-16 08:37:21 +0200

    SP-2 ADD Dependency injection into the @ShellComponent SpringShell app

--- src/main/java/com/pet/learn_spring/LearnSpringApplication.java
+++ src/main/java/com/pet/learn_spring/LearnSpringApplication.java
@@ -1,20 +1,11 @@
 package com.pet.learn_spring;
 
-import com.pet.learn_spring.core.FileSystem;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.util.unit.DataSize;
 
 @SpringBootApplication
 public class LearnSpringApplication {
 	public static void main(String[] args) {
-		ApplicationContext app = SpringApplication.run(LearnSpringApplication.class, args);
-
-		FileSystem fileSystem = app.getBean(FileSystem.class);
-
-		System.out.println(
-				DataSize.ofBytes(fileSystem.getFreeDiskSpace()).toGigabytes() + " GB"
-		);
+		SpringApplication.run(LearnSpringApplication.class, args);
 	}
 }
