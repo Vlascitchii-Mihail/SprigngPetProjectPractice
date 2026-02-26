207b807359754c1ccb04a7f1e649eac1ceedbe27
Mihail <mishavlashitschii@gmail.com>
2026-02-26 12:52:27 +0200

    SP-4 Abstract Injection

--- src/main/java/com/pet/learn_spring/core/photo/PhotoService.java
+++ src/main/java/com/pet/learn_spring/core/photo/PhotoService.java
@@ -1,19 +1,28 @@
 package com.pet.learn_spring.core.photo;
 
 import com.pet.learn_spring.core.FileSystem;
+import com.pet.learn_spring.core.photo.thumbnail.Thumbnail;
+import com.pet.learn_spring.core.photo.thumbnail.ThumbnailRendering;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Service;
 
 import java.io.UncheckedIOException;
 import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class PhotoService {
 
     private final FileSystem fileSystem;
+    private final Thumbnail thumbnail;
 
-    public PhotoService(@NotNull FileSystem fileSystem) {
+    public PhotoService(
+            @NotNull FileSystem fileSystem,
+            @ThumbnailRendering(ThumbnailRendering.RenderQuality.FAST)
+            Thumbnail thumbnail
+    ) {
         this.fileSystem = fileSystem;
+        this.thumbnail = thumbnail;
     }
 
     public Optional<byte[]> download(String name) {
@@ -22,5 +31,15 @@ public class PhotoService {
         } catch(UncheckedIOException ex) {
             return Optional.empty();
         }
+    }
+
+    public String upload(byte[] imageBytes) {
+        String imageName = UUID.randomUUID().toString();
+
+        fileSystem.store(imageName + ".jpg", imageBytes);
+        byte[] thumbnailBytes = thumbnail.thumbnail(imageBytes);
+        fileSystem.store(imageName + "-thumb.jpg", thumbnailBytes);
+
+        return imageName;
     }
 }
