611905f67444dce4a9b1f98e7c660be4e1d1575b
Mihail <mishavlashitschii@gmail.com>
2026-02-26 12:52:27 +0200

    SP-4 Abstract Injection

---
+++ src/main/java/com/pet/learn_spring/core/photo/thumbnail/AwtNearestNeighborThumbnail.java
@@ -0,0 +1,46 @@
+package com.pet.learn_spring.core.photo.thumbnail;
+
+import org.springframework.stereotype.Service;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.*;
+
+@Service()
+@ThumbnailRendering(ThumbnailRendering.RenderQuality.FAST)
+public class AwtNearestNeighborThumbnail implements Thumbnail{
+
+    private static BufferedImage create(BufferedImage source, int width, int height) {
+        double thumbRatio = (double) width / height;
+        double imageRatio = (double) source.getWidth() / source.getHeight();
+
+        if ( thumbRatio < imageRatio ) height = (int) (width / imageRatio);
+        else width = (int) (height * imageRatio);
+
+        BufferedImage thumb = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+
+        Graphics2D g2 = thumb.createGraphics();
+        g2.setRenderingHint(
+                RenderingHints.KEY_INTERPOLATION,
+                RenderingHints.VALUE_INTERPOLATION_NEAREST_NEIGHBOR
+        );
+        g2.drawImage(source, 0, 0, width, height, null);
+        g2.dispose();
+
+        return thumb;
+    }
+
+    @Override
+    public byte[] thumbnail(byte[] imageBytes) {
+        try {
+            InputStream is = new ByteArrayInputStream(imageBytes);
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            BufferedImage thumbnail = create(ImageIO.read(is), 200, 200);
+            ImageIO.write(thumbnail, "jpg", baos);
+            return baos.toByteArray();
+        } catch (IOException ex) {
+            throw new UncheckedIOException(ex);
+        }
+    }
+}
---
+++ src/main/java/com/pet/learn_spring/core/photo/thumbnail/Thumbnail.java
@@ -0,0 +1,6 @@
+package com.pet.learn_spring.core.photo.thumbnail;
+
+public interface Thumbnail {
+
+    byte[] thumbnail(byte[] imageBytes);
+}
